67b3ffd230c1b43d77f167ab0706c77107392caa
Josh Ford <thisisjoshford@gmail.com>
2020-01-15 17:25:59 -0800

    basic html framework

---
+++ app.js
@@ -0,0 +1,9 @@
+const shootButton = document.getElementById('shootButton');
+const userChoice = document.getElementById('choice').value;
+
+
+shootButton.addEventListener('click', () => {
+    console.log(userChoice);
+});
+
+
